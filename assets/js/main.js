25bae164811b0bc119775d53b7829880d58c669e
Mikhail Belovolov <don.belovolov2018@yandex.ru>
2021-08-07 20:54:31 +0300

    in process

--- assets/js/main.js
+++ assets/js/main.js
@@ -74,30 +74,52 @@ $('.hero-slide').slick({
 });
 
 /*  VALIDATE */
-$.validator.addMethod('regex', function(value, element,regexp){
-  let regExsp = new RegExp(regexp);
-  return this.optional(element) || regExsp.test(value)
-}, 'please check your input');
 
-$('form').validate({
-  rules : {
-    firstName : {
-      reguired : true,
-      regex : "[A-Za-z]{1,32}"
+$.validator.addMethod("regex", function(value, element, regexp) {
+  var regExsp = new RegExp(regexp);
+  return regExsp.test(value);
+},"Please check your input."
+);
+
+$("form").validate({
+  rules: {
+    firstName: {
+      required: true,
+      regex : "[A-Za-z]{1,32}"   
     },
-    phoneNumber : {
-      reguired : true,
+    phoneNumber: {
       digits : true,
-      minlength : 10,
-      maxlength : 11,
-      regex : "[0-9]+"
+      required: true,
+      minlength: 10,
+      maxlength: 11,
+      regex: "[0-9]+"
     }
   },
-  messages : {
-    firstName : `введите имя правильно`,
-    phoneNumber : `Введите правильный ваш номер`
+  messages: {
+    firstName: "Введите ваше имя правильно",
+    phoneNumber: "Введите ваш номер"
   }
-})
+});
+
+/*  map  */
+ymaps.ready(init);
+  function init(){
+    let myMap = new ymaps.Map("map", {
+      center: [56.31, 44.01],
+      zoom: 10,
+      controls: ['zoomControl','geolocationControl']
+    });
+
+    let myPlacemark = new ymaps.Placemark([56.31,44.01],{} , {
+      iconImageSize : [32, 40]
+    })
+
+    myMap.balloon.open([51,85, 38,37], 'Содержание балуна', {
+        closeButton: false
+    })
+
+    myMap.geoObjects.add(myPlacemark);
+  }
 
 
 
