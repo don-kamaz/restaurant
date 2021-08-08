9b776703c67046f9c12821203a661f2e4eee2048
Mikhail Belovolov <don.belovolov2018@yandex.ru>
2021-08-08 10:38:57 +0300

    in process

--- assets/js/main.js
+++ assets/js/main.js
@@ -100,34 +100,3 @@ $("form").validate({
     phoneNumber: "Введите ваш номер"
   }
 });
-
-/*  map  */
-ymaps.ready(init);
-  function init(){
-    let myMap = new ymaps.Map("map", {
-      center: [56.31, 44.01],
-      zoom: 10,
-      controls: ['zoomControl','geolocationControl']
-    });
-
-    let myPlacemark = new ymaps.Placemark([56.31,44.01],{} , {
-      iconImageSize : [32, 40]
-    })
-
-    myMap.balloon.open([51,85, 38,37], 'Содержание балуна', {
-        closeButton: false
-    })
-
-    myMap.geoObjects.add(myPlacemark);
-  }
-
-
-
-
-
-
-
-
-
-
-
